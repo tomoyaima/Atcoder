3cca8c57091f87ff18180f9d80bb81d2b68346e2
今村友哉 <imatomo1234imatomo@gmail.com>
2020-05-31 23:06:17 +0900

    0531

---
+++ atcider05312.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+#include <sstream>
+#include <stdio.h>
+#include <math.h>
+ 
+using namespace std;
+ 
+int main() {
+    
+    
+  
+    long long int a;
+    long double b;
+      cin >> a >> b;
+    long long int sum;
+    sum = (a*b);
+   
+  
+    cout << sum << endl;
+    
+} 
